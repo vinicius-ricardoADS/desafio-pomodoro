f23bb404335d432608e57be1b4b4fecd2eb3a1e6
Vinicius Ricardo Rosa Noli <viniciusnoli2016@gmail.com>
2024-07-26 16:07:43 -0300

    feat: integrated database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { PomodorosModule } from './controllers/pomodoros.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pomodoro } from './entities/pomodoro.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [PomodorosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: 'pomodoro',
+      entities: [Pomodoro],
+      synchronize: true,
+    }),
+    PomodorosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/entities/pomodoro.entity.ts
+++ src/entities/pomodoro.entity.ts
@@ -1,7 +1,19 @@
+import { Column, Entity } from 'typeorm';
+
+@Entity()
 export class Pomodoro {
+  @Column({ primary: true })
   id: string;
+
+  @Column()
   description: string;
+
+  @Column()
   numberOfPomodoros: number;
+
+  @Column({ default: 25 })
   pomodoroTime: number;
+
+  @Column({ default: 5 })
   pomodoroBreak: number;
 }
